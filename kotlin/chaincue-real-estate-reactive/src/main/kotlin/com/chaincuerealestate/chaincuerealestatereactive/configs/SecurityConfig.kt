0ae87193673f94aac589bb8b8aa70a533060eb30
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-11-16 20:47:21 +0100

    commit

--- kotlin/chaincue-real-estate-reactive/src/main/kotlin/com/chaincuerealestate/chaincuerealestatereactive/configs/SecurityConfig.kt
+++ kotlin/chaincue-real-estate-reactive/src/main/kotlin/com/chaincuerealestate/chaincuerealestatereactive/configs/SecurityConfig.kt
@@ -1,44 +1,46 @@
 package com.chaincuerealestate.chaincuerealestatereactive.configs
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.convert.converter.Converter
-//import org.springframework.security.authentication.AbstractAuthenticationToken
-//import org.springframework.security.config.Customizer
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-//import org.springframework.security.config.web.server.ServerHttpSecurity
-//import org.springframework.security.oauth2.jwt.Jwt
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
-//import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
-//import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
+import org.springframework.security.web.server.SecurityWebFilterChain
 import reactor.core.publisher.Mono
 
-//@Configuration
-//@EnableWebFluxSecurity
+@Configuration
+@EnableWebFluxSecurity
 @Profile("!test")
 class SecurityConfig {
 
-//    @Bean
-//    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-//        return http
-//            .cors(Customizer.withDefaults())
-//            .authorizeExchange { exchange ->
-//                exchange
-//                    .pathMatchers("/home").permitAll()
-//                    .pathMatchers("/houses").permitAll()
-//                    .pathMatchers("/house/**").permitAll()
-//                    .pathMatchers("/account").hasRole("user")
-//                    .anyExchange().authenticated()
-//            }
-//            .oauth2ResourceServer { oauth2 -> oauth2.jwt { jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()) } }
-//            .build()
-//    }
-//
-//    private fun jwtAuthenticationConverter(): Converter<Jwt, out Mono<out AbstractAuthenticationToken>> {
-//        val jwtConverter = JwtAuthenticationConverter()
-//        jwtConverter.setJwtGrantedAuthoritiesConverter(KeycloakRealmRoleConverter())
-//        return ReactiveJwtAuthenticationConverterAdapter(jwtConverter)
-//    }
-}
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .cors(Customizer.withDefaults())
+            .authorizeExchange { exchange ->
+                exchange
+                    .pathMatchers("/home").permitAll()
+                    .pathMatchers("/houses").permitAll()
+                    .pathMatchers("/house/**").permitAll()
+                    .pathMatchers("/account").hasRole("user")
+                    .anyExchange().authenticated()
+            }
+            .oauth2ResourceServer { oauth2 -> oauth2.jwt { jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()) } }
+            .build()
+    }
 
+    private fun jwtAuthenticationConverter(): Converter<Jwt, out Mono<out AbstractAuthenticationToken>> {
+        val jwtConverter = JwtAuthenticationConverter()
+        jwtConverter.setJwtGrantedAuthoritiesConverter(KeycloakRealmRoleConverter())
+        return ReactiveJwtAuthenticationConverterAdapter(jwtConverter)
+    }
+}
--- kotlin/chaincue-real-estate-reactive/src/test/kotlin/com/chaincuerealestate/chaincuerealestatereactive/routes/home/HomePageTest.kt
+++ kotlin/chaincue-real-estate-reactive/src/test/kotlin/com/chaincuerealestate/chaincuerealestatereactive/routes/home/HomePageTest.kt
@@ -1,25 +1,20 @@
 package com.chaincuerealestate.chaincuerealestatereactive.routes.home
 
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.RepeatedTest
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
-import org.springframework.test.annotation.Repeat
-import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HomePageTest {
 
     @Autowired
     private lateinit var homePage: HomePage
 
     @RepeatedTest(100)
-    fun `should return home page DTO`() {
+    fun `should return home page DTO`()  {
         runBlocking {
             homePage.homePage()
         }
